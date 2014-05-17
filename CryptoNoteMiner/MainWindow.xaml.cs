9dc5de09e451115aa6c8b1a4fe11cfa1108592ca
Jojatekok <jojatekokweb@gmail.com>
2014-05-17 12:08:51 +0200

    Added wallet file generation

--- CryptoNoteMiner/MainWindow.xaml.cs
+++ CryptoNoteMiner/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +17,105 @@ using System.Windows.Shapes;
 
 namespace CryptoNoteMiner
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        internal string BasePath { get; private set; }
+
+        internal const string RelativePathWalletData = "wallet";
+        internal const string RelativePathWalletAddress = "wallet.address.txt";
+
+        internal string RelativePathResources { get; private set; }
+        internal const string RelativePathResourceSimpleminer = "simpleminer.exe";
+        internal const string RelativePathResourceSimplewallet = "simplewallet.exe";
+
+        internal string WalletAddress { get; private set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            BasePath = AppDomain.CurrentDomain.BaseDirectory;
+            RelativePathResources = Environment.Is64BitOperatingSystem ?
+                                    @"Resources\64-bit\" :
+                                    @"Resources\32-bit\";
+
+
+            var currentFile = BasePath + RelativePathResources + RelativePathResourceSimpleminer;
+            if (!File.Exists(currentFile)) MessageManager.ShowError("Missing file: " + currentFile);
+
+            currentFile = BasePath + RelativePathResources + RelativePathResourceSimplewallet;
+            if (!File.Exists(currentFile)) MessageManager.ShowError("Missing file: " + currentFile);
+
+            currentFile = BasePath + RelativePathResources + RelativePathWalletAddress;
+            if (File.Exists(currentFile)) {
+                ReadWalletAddress();
+            } else {
+                MessageManager.ShowInformation("Generating new wallet with password: x");
+                GenerateWallet();
+            }
+
+            var coresAvailable = Environment.ProcessorCount;
+            for (var i = coresAvailable - 1; i >= 0; i--) {
+                ComboBoxCpuCores.Items.Add(i + 1);
+            }
+
+            //var coresConfig = INI.Value("cores");
+            //int coresInt = comboBoxCores.Items.Count - 1;
+            //if (coresConfig != "")
+            //{
+            //    int coresParsed;
+            //    var parsed = int.TryParse(coresConfig, out coresParsed);
+            //    if (parsed) coresInt = coresParsed - 1;
+            //    if (coresInt + 1 > coresAvailable) coresInt = coresAvailable - 1;
+
+            //}
+            //comboBoxCores.SelectedIndex = coresInt;
+
+            //var poolHost = INI.Value("pool_host");
+            //if (poolHost != "")
+            //{
+            //    textBoxPoolHost.Text = poolHost;
+            //}
+            //var poolPort = INI.Value("pool_port");
+            //if (poolPort != "")
+            //{
+            //    textBoxPoolPort.Text = poolPort;
+            //}
+
+            //Application.ApplicationExit += (s, e) => killMiners();
+        }
+
+        private void ReadWalletAddress()
+        {
+            WalletAddress = File.ReadAllText(BasePath + RelativePathWalletAddress);
+            TextBoxWalletAddress.Text = WalletAddress;
+        }
+
+        private void GenerateWallet()
+        {
+            var arguments = new[] {
+                "--generate-new-wallet=\"" + BasePath + RelativePathWalletData + "\"",
+                "--password=x"
+            };
+
+            var process = new Process {
+                EnableRaisingEvents = true,
+                StartInfo = new ProcessStartInfo(BasePath + RelativePathResources + RelativePathResourceSimplewallet, string.Join(" ", arguments)) {
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true
+                }
+            };
+
+            process.Exited += (sender, e) => {
+                if (!File.Exists(BasePath + RelativePathWalletAddress)) {
+                    Dispatcher.Invoke(() => MessageManager.ShowError("Failed to generate new wallet."));
+                } else {
+                    Dispatcher.Invoke(ReadWalletAddress);
+                }
+            };
+
+            process.Start();
         }
     }
 }
---
+++ CryptoNoteMiner/MessageManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace CryptoNoteMiner
+{
+    static class MessageManager
+    {
+        internal static void ShowInformation(string message)
+        {
+            MessageBox.Show(message, "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        internal static void ShowError(string message)
+        {
+            MessageBox.Show(message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+
+    }
+}
